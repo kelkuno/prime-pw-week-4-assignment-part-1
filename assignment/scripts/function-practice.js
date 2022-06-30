42ea6fcb99966c07ed2d53841ba100a7dab4f5e9
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-06-30 18:39:23 -0500

    q 9 done.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -112,9 +112,7 @@ console.log(find( 9, otherArray));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
-let testString = "Kelsey";
-//testString.split();
-//console.log(testString.split(''));
+
 function isFirstLetter(letter, string) {
   let i = 0;
   let letterMatch = false;
@@ -143,11 +141,21 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+
+let someArray = [1, 3, 5, 6];
+
+function sumAll( array ) {
+  i = 0;
   let sum = 0
-  // TODO: loop to add items
+  console.log('in the summAll function', array);
+  while( i<array.length){
+    sum += array[i];
+    i++;
+  }//end of loop
   return sum;
-}
+}//end of sumAll function
+
+console.log('Here is the sum to the array:', sumAll(someArray));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
